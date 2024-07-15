82ae3a3ae4b4e7ab906fe5a7fecedf48c842577a
Awoke Gebrie <awokegebrie5@gmail.com>
2024-07-15 19:21:12 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import About from './comp/About.js';
+import Home from './comp/Contact';
+import Home from './comp/Home.js';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { BrowserRouter  as Router,Routes,Route,Link } from "react-router-dom";
+function App(){
+  return(
+    <div>
+      <Router>
+        <nav>
+        <Link className="nav"to='/'>Home</Link>
+        <Link className="nav"to='/About'>About</Link>
+        </nav>
+        
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/About' element={<About/>}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
-
 export default App;
+
+
+
+
+// const UserProfile = ({ userType }) => {
+//   let userDetails;
+
+//   switch (userType) {
+//     case 'admin':
+//       userDetails = <AdminProfile />;
+//       break;
+//     case 'manager':
+//       userDetails = <ManagerProfile />;
+//       break;
+//     case 'receptionist':
+//       userDetails = <ReceptionistProfile />;
+//       break;
+//     case 'content_creator':
+//       userDetails = <ContentCreatorProfile />;
+//       break;
+//     default:
+//       userDetails = <GuestProfile />;
+//   }
+
+//   return (
+//     <div>
+//       {userDetails}
+//       <index/>
+//     </div>
+//   );
+// };
+
+// const AdminProfile = () => <h2>Admin Profile</h2>;
+// const ManagerProfile = () => <h2>Manager Profile</h2>;
+// const ReceptionistProfile = () => <h2>Receptionist Profile</h2>;
+// const ContentCreatorProfile = () => <h2>Content Creator Profile</h2>;
+// const GuestProfile = () => <h2>Guest Profile</h2>;
+
+
---
+++ src/comp/Home.js
@@ -0,0 +1,10 @@
+function Home(){
+    return (
+        <div>
+            <h1>This is Reception</h1>
+        </div>
+        
+    )
+  }
+  
+  export default Home;
